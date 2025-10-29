b10ded49e6efbb7a873afeb5191bf47817611290
tayqiter00 <tayqiter00@gmail.com>
2025-10-30 02:44:49 +0800

    chore(backend): set up database
    DB created and seeded using sample data with Prisma ORM

---
+++ backend/prisma/seed.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from '../generated/prisma/index.js'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      { email: 'alice@example.com', name: 'Alice' },
+      { email: 'bob@example.com', name: 'Bob' },
+      { email: 'charlie@example.com', name: 'Charlie' },
+    ],
+  })
+
+  console.log('✅ Database has been seeded')
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
