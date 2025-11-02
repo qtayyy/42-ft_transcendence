4d16474c6afcaa956b08b0a2ed2f32d9d2db91c8
tayqiter00 <tayqiter00@gmail.com>
2025-11-03 01:55:01 +0800

    chore(backend): Use Prettier formatting

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-import path from 'node:path'
-import AutoLoad from '@fastify/autoload'
-import { fileURLToPath } from 'node:url'
+import path from "node:path";
+import AutoLoad from "@fastify/autoload";
+import { fileURLToPath } from "node:url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Pass --options via CLI arguments in command to enable these options.
-export const options = {}
+export const options = {};
 
 export default async function (fastify, opts) {
   // Place here your custom code!
@@ -14,24 +14,25 @@ export default async function (fastify, opts) {
     console.error(error);
 
     reply.status(500).send({
-      message: 'Something went wrong!',
-      error: error.message || 'Unknown error',
+      message: "Something went wrong!",
+      error: error.message || "Unknown error",
     });
   });
+
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
 }
