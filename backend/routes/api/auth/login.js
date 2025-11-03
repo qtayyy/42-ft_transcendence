71824a287ae6e35d41f03c6bce9a45840c003d4d
tayqiter00 <tayqiter00@gmail.com>
2025-11-04 00:59:21 +0800

    feat(jwt): new JWT validator for 2fa verification
    Read the file comments in login

--- backend/routes/api/auth/login.js
+++ backend/routes/api/auth/login.js
@@ -19,30 +19,54 @@ export default async function (fastify, opts) {
       if (match === false)
         return reply.code(400).send({ error: "Incorrect email or password" });
 
-      if (user.twoFA === true) {
-        return reply.code(401).send({
-          message: "2FA enabled, please complete the authentication process",
-          twofaUrl: "/api/auth/2fa/verify",
-        });
+      // If user enabled 2FA, we send them a temporary JWT that will only be
+      // checked by the /api/auth/2fa/verify route. This is needed to check 
+      // users that access the 'verify 2fa' route has at least passed the
+      // initial login phase. In other words, the 'verify 2fa' route is
+      // protected by the temporary JWT. Note that this temporary JWT CANNOT
+      // be used to access other protected routes.
+      //
+      // We store the JWT in a cookie to reduce the attack surface of XSS.
+      // Read more on: https://github.com/fastify/fastify-jwt
+      if (user.twoFA) {
+        const token = fastify.jwt.temp.sign(
+          { userId: user.id },
+          { expiresIn: "5m" }
+        );
+        return reply
+          .setCookie("token", token, {
+            path: '/api/auth/2fa/verify', // double check
+            secure: true,
+            httpOnly: true,
+            sameSite: true,
+          })
+          .code(401) // Or 200?
+          .send({
+            message: "2FA enabled, please complete the authentication process",
+            twofaUrl: "/api/auth/2fa/verify",
+          });
       } else {
+        // Else, we straight away assign the full JWT that is used to protect
+        // all other sensitive routes.
         const token = fastify.jwt.sign(
-          {
-            userId: user.id,
-            email: user.email,
-          },
-          {
-            expiresIn: "1h",
-          }
+          { userId: user.id },
+          { expiresIn: "1h" }
         );
 
-        return reply.code(200).send({
-          message: "Login successful",
-          token,
-          user: {
-            userId: user.id,
-            email: user.email,
-          },
-        });
+        return reply
+          .setCookie("token", token, {
+            path: '/',
+            secure: true,
+            httpOnly: true,
+            sameSite: true,
+          })
+          .code(200)
+          .send({
+            message: "Login successful",
+            user: {
+              userId: user.id,
+            },
+          });
       }
     } catch (error) {
       console.error("Error occurred during login:", error);
