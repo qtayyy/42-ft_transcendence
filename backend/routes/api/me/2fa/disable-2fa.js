4f0d0f3c056c6e170629f2bb0a70d57642661342
tayqiter00 <tayqiter00@gmail.com>
2025-11-04 01:02:53 +0800

    fix(2fa): re-organize 2fa routes
    Previous routes allow different users to modify each other's 2FA settings

--- backend/routes/api/me/2fa/disable-2fa.js
+++ backend/routes/api/me/2fa/disable-2fa.js
@@ -2,25 +2,19 @@ import { PrismaClient } from "../../../generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
+// Double check that user A cannot access user B's page
 export default async function (fastify, opts) {
   fastify.get(
-    "/:userId/2fa/disable",
+    "/disable",
     {
       onRequest: [fastify.authenticate],
     },
     async (request, reply) => {
       try {
-        const userId = Number(request.params.userId);
-
-        if (Number.isNaN(userId)) {
-          return reply.code(400).send({ error: "Invalid user ID" });
-        }
+        const userId = request.user.userId;
 
         const user = await prisma.user.findUnique({ where: { id: userId } });
         if (!user) return reply.code(404).send({ error: "User not found" });
-        if (request.user.id !== userId) {
-          return reply.code(403).send({ error: "Forbidden" });
-        }
         if (!user.twoFA)
           return reply.code(400).send({ error: "2FA already disabled" });
 
@@ -36,7 +30,7 @@ export default async function (fastify, opts) {
         });
         return reply.code(200).send({ message: "2FA successfully disabled" });
       } catch (error) {
-        fastify.log.error("Error occurred during 2FA:", error);
+       fastify.log.error("Error occurred disabling 2FA:", error);
         return reply.code(500).send({ error: "Internal server error" });
       }
     }
