9a1c92a007615987fc0a40a2b5a7f3bcd1f3be8e
tayqiter00 <tayqiter00@gmail.com>
2025-11-03 17:31:06 +0800

    chore(login): add try-catch for login

--- backend/routes/api/auth/login.js
+++ backend/routes/api/auth/login.js
@@ -5,46 +5,48 @@ const prisma = new PrismaClient();
 
 export default async function (fastify, opts) {
   fastify.post("/login", async (request, reply) => {
-    const { email, password } = request.body;
+    try {
+      const { email, password } = request.body;
 
-    if (!email || !password)
-      return reply.code(400).send({ error: "Missing fields" });
+      if (!email || !password)
+        return reply.code(400).send({ error: "Missing fields" });
 
-    const user = await prisma.user.findUnique({ where: { email } });
+      const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!user)
-      return reply.code(400).send({ error: "Incorrect email or password" });
-    const match = await bcrypt.compare(password, user.password);
-    if (match === false)
-      return reply.code(400).send({ error: "Incorrect email or password" });
+      if (!user)
+        return reply.code(400).send({ error: "Incorrect email or password" });
+      const match = await bcrypt.compare(password, user.password);
+      if (match === false)
+        return reply.code(400).send({ error: "Incorrect email or password" });
 
-    const twofaEnabled = user.twoFA;
+      if (user.twoFA === true) {
+        return reply.code(401).send({
+          message: "2FA enabled, please complete the authentication process",
+          twofaUrl: "/api/auth/2fa/verify",
+        });
+      } else {
+        const token = fastify.jwt.sign(
+          {
+            userId: user.id,
+            email: user.email,
+          },
+          {
+            expiresIn: "1h",
+          }
+        );
 
-    if (twofaEnabled) {
-      return reply.code(401).send({
-        message: "2FA enabled, please complete the authentication process",
-        twofaUrl: "/api/auth/2fa/verify"
-      });
-    }
-    else {
-      const token = fastify.jwt.sign(
-        {
-          userId: user.id,
-          email: user.email,
-        },
-        {
-          expiresIn: "1h",
-        }
-      );
-
-      return reply.code(200).send({
-        message: "Login successful",
-        token,
-        user: {
-          userId: user.id,
-          email: user.email,
-        },
-      });
+        return reply.code(200).send({
+          message: "Login successful",
+          token,
+          user: {
+            userId: user.id,
+            email: user.email,
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Error occurred during login:", error);
+      return reply.code(500).send({ error: "Internal server error" });
     }
   });
 }
