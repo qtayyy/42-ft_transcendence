de250be06f739e5a2fbe7b2b41a52ece29e23bf4
tayqiter00 <tayqiter00@gmail.com>
2025-11-05 23:34:00 +0800

    feat(frontend): add layout for root page

---
+++ frontend/app/ui/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer()
+{
+  return (
+    <div className="sticky bottom-0 w-screen dark:bg-background p-3">
+      <p className="text-gray-100 text-end">Switch Language</p>
+    </div>
+  );  
+}
