8ed7473c23675a444b16c457c4f2424b1fce180f
tayqiter00 <tayqiter00@gmail.com>
2025-11-11 00:44:17 +0800

    feat(auth): Add logout function

--- frontend/app/ui/header.tsx
+++ frontend/app/ui/header.tsx
@@ -1,14 +1,30 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { AuthDialog } from "./auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-
-export default function Header()
-{
+export default function Header() {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    try {
+      axios.post("/api/auth/logout");
+      router.push("/");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
 
   return (
     <div className="sticky top-0 flex justify-between p-3 items-center dark:bg-background z-50">
@@ -24,19 +40,30 @@ export default function Header()
         <p className="text-gray-100">Logo placeholder</p>
       </div>
       <div className="flex space-x-5">
-        {
-          pathname === '/' ?
+        {pathname === "/2fa/verify" ? null : pathname === "/" ? (
           <>
             <AuthDialog text="Sign In" />
             <AuthDialog text="Get Started" />
-          </> :
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        }
-          
-       </div>
+          </>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Match History</DropdownMenuItem>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem onClick={handleLogOut}>
+                Log Out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
     </div>
-  );  
+  );
 }
