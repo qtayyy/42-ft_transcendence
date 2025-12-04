5628211caca4556c31d73e5fc7a133b22c41d836
nelsonwm45 <nelsonwm45@gmail.com>
2025-12-04 21:41:45 +0800

    style(colour): Forced dark mode

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system" // plan : "dark"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
