70826fa8d40020f91b46a1256423d38a59b29d12
nelsonwm45 <nelsonwm45@gmail.com>
2025-12-04 18:07:20 +0800

    docs(theme): added a comment to force change theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="system" // plan : "dark"
           enableSystem
           disableTransitionOnChange
         >
